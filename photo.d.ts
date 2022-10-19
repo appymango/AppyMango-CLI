30376fc3e385a6308d24b940c77d55302d5abc74
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2022-10-19 12:11:52 +0200

    FEAT: Default configs added

---
+++ photo.d.ts
@@ -0,0 +1,10 @@
+//To fix local assets imports error on typescript
+declare module '.*png' {
+  const value: any;
+  export = value;
+}
+
+declare module '.*jpg' {
+  const value: any;
+  export = value;
+}
