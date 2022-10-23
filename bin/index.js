e7bd19ae5e897e7c59a4b563a0b73b0885537f41
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2022-10-23 10:32:08 +0200

    FEAT: Working file transfer added

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,9 @@ const chalk = require("chalk");
 const boxen = require("boxen");
 const yargs = require("yargs");
 const figlet = require("figlet");
+// const fs = require("fs");
+const path = require("path");
+const fs = require("fs-extra");
 
 const usage = chalk.hex("#FFC942")(
   "\nUsage: mango -f <Folder Name> \n",
@@ -36,12 +39,26 @@ const options = yargs
 // console.log(yargs.argv);
 const argv = require("yargs/yargs")(process.argv.slice(2)).argv;
 
-if (argv.folder == null && argv.f == null) {
+if (argv.folder !== null && argv.f !== null) {
   console.log(
     chalk.yellow(figlet.textSync("mango", { horizontalLayout: "full" }))
   );
+  let src = path.resolve(__dirname, "../rn-typescript");
+  let dest = process.cwd();
+
+  try {
+    fs.copySync(src, dest, { overwrite: true });
+    console.log("success!");
+  } catch (err) {
+    console.error(err);
+  }
+
+  // fs.copySync(path.resolve(__dirname, "../rn-typescript"), process.cwd(), {
+  //   overwrite: true,
+  // });
+
   yargs.showHelp();
-  //   return;
+  return;
 }
 if (argv.config == null && argv.c == null) {
   yargs.showHelp();
@@ -55,15 +72,15 @@ const config = argv.c || argv.config;
 // console.log( language,sentence);
 // translate(sentence, { to: language.toLowerCase() })
 //   .then((res) => {
-console.log(
-  "\n" +
-    boxen(chalk.green(sentence + "\n\n" + language), {
-      padding: 1,
-      borderColor: "green",
-      dimBorder: true,
-    }) +
-    "\n"
-);
+// console.log(
+//   "\n" +
+//     boxen(chalk.green(sentence + "\n\n" + language), {
+//       padding: 1,
+//       borderColor: "green",
+//       dimBorder: true,
+//     }) +
+//     "\n"
+// );
 //   })
 //   .catch((err) => {
 //     console.error(err);
