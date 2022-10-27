9665dd7d36357fcd7fc5b18ef77c1f2808933777
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2022-10-27 17:01:50 +0200

    FEAT: All frameworks added

--- dependencies/react/javascript/dependencies.js
+++ dependencies/react/javascript/dependencies.js
@@ -1,18 +1,17 @@
-const dependencies = ["axios"];
+const dependencies = ['axios', 'patch-package', 'postinstall-postinstall'];
 
 const devDependencies = [
-  "@commitlint/cli",
-  "@commitlint/config-conventional",
-  "cspell",
-  "lint-staged",
-  "prettier",
-  "eslint-plugin-unused-imports",
-  "eslint-plugin-no-relative-import-paths",
-  "eslint-plugin-ft-flow",
-  "@svgr/cli",
-  "eslint-plugin-react",
-  "@babel/eslint-parser",
-  "babel-plugin-module-resolver",
+  '@commitlint/cli',
+  '@commitlint/config-conventional',
+  'cspell',
+  'lint-staged',
+  'prettier',
+  'eslint-plugin-unused-imports',
+  'eslint-plugin-no-relative-import-paths',
+  'eslint-plugin-ft-flow',
+  '@svgr/cli',
+  'eslint-plugin-react',
+  '@trivago/prettier-plugin-sort-imports',
 ];
 
-module.exports = { devDependencies, dependencies };
+module.exports = {devDependencies, dependencies};
--- dependencies/react/typescript/dependencies.js
+++ dependencies/react/typescript/dependencies.js
@@ -1,19 +1,16 @@
-const dependencies = ["axios"];
+const dependencies = ['axios', 'patch-package', 'postinstall-postinstall'];
 
 const devDependencies = [
-  "@commitlint/cli",
-  "@commitlint/config-conventional",
-  "cspell",
-  "husky",
-  "lint-staged",
-  "prettier",
-  "eslint-plugin-unused-imports",
-  "eslint-plugin-no-relative-import-paths",
-  "eslint-plugin-ft-flow",
-  "@svgr/cli",
-  "eslint-plugin-react",
-  "@babel/eslint-parser",
-  "babel-plugin-module-resolver",
+  '@commitlint/cli',
+  '@commitlint/config-conventional',
+  'cspell',
+  'lint-staged',
+  'prettier',
+  'eslint-plugin-unused-imports',
+  'eslint-plugin-no-relative-import-paths',
+  'eslint-plugin-ft-flow',
+  '@svgr/cli',
+  'eslint-plugin-react',
 ];
 
-module.exports = { devDependencies, dependencies };
+module.exports = {devDependencies, dependencies};
