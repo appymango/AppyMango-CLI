5d04ef22bfc3ededa9468557571fecc79223fed9
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2023-01-03 15:28:07 +0100

    UPDATE: Javascript dead code detection added

--- dependencies/react-native/javascript/dependencies.js
+++ dependencies/react-native/javascript/dependencies.js
@@ -1,1 +1,26 @@
-const dependencies=["react-native-dotenv","axios","patch-package","postinstall-postinstall"],devDependencies=["@commitlint/cli","@commitlint/config-conventional","cspell","lint-staged","prettier","react-native-svg-transformer","eslint-plugin-unused-imports","eslint-plugin-no-relative-import-paths","eslint-plugin-ft-flow","@svgr/cli","eslint-plugin-react","eslint-plugin-react-native","@babel/eslint-parser","babel-plugin-module-resolver","babel-eslint","@trivago/prettier-plugin-sort-imports"];module.exports={devDependencies:devDependencies,dependencies:dependencies};
+const dependencies = [
+    'react-native-dotenv',
+    'axios',
+    'patch-package',
+    'postinstall-postinstall',
+  ],
+  devDependencies = [
+    '@commitlint/cli',
+    '@commitlint/config-conventional',
+    'cspell',
+    'lint-staged',
+    'prettier',
+    'react-native-svg-transformer',
+    'eslint-plugin-unused-imports',
+    'eslint-plugin-no-relative-import-paths',
+    'eslint-plugin-ft-flow',
+    '@svgr/cli',
+    'eslint-plugin-react',
+    'eslint-plugin-react-native',
+    '@babel/eslint-parser',
+    'babel-plugin-module-resolver',
+    'babel-eslint',
+    '@trivago/prettier-plugin-sort-imports',
+    'eslint-plugin-import',
+  ];
+module.exports = {devDependencies: devDependencies, dependencies: dependencies};
